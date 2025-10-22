b31f3985bb3be45c6d2d8f74f42de80108e4b6c4
Fatima Zahra <fzahra@uoguelph.ca>
2025-10-22 12:27:56 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -15,7 +15,7 @@ def generate_fibonacci(n):
   #Generate Fibonacci sequence up to n terms and return list
   sequence = []
   a, b = 0, 1
-  for _ in range(n):
+  for i in range(n):
     sequence.append(a)
     a, b = b, a + b
   return sequence
